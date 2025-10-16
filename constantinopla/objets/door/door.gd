085d8a10482389e2f58de06552cd20490efcb03f
Ana Nemer <d48457362@alumnos.itr.edu.ar>
2025-10-16 16:03:40 -0300

    dsd

--- constantinopla/objets/door/door.gd
+++ constantinopla/objets/door/door.gd
@@ -3,6 +3,8 @@ extends StaticBody2D
 var si_esta_cerca = false
 const DIALOGO_door = preload("res://Dialogos/text/door.dialogue")
 
+@onready var exclamation=$Exclamation
+
 func _process(delta: float) -> void:
 	if si_esta_cerca and Input.is_action_just_pressed("talk") and GlobalValues.llave:
 		self.set_collision_layer_value(1,false)
@@ -12,7 +14,8 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	si_esta_cerca = true
-
+	exclamation.visible=true
 
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	si_esta_cerca = false
+	exclamation.visible=false
